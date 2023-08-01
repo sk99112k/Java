8b1c42cf124a8bded4312453c17d9ec64953d02c
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-01 17:48:56 +0900

    while文

---
+++ 繰り返し処理/main13.java
@@ -0,0 +1,15 @@
+package 繰り返し処理;
+  class main13 {
+    public static void main(String[] args) {
+      int number = 10;
+      
+      // while文を用いて、numberが0より大きい場合に繰り返す、繰り返し処理を作ってください
+      while (number > 0) {
+        System.out.println(number);
+        number--;
+      }
+      
+  }
+}
+
+// while文
