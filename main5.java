52cccca4797d474bee7bc7d7756bd018905c0fa6
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-08-01 06:34:29 +0000

    整数型 総合練習

---
+++ main5.java
@@ -0,0 +1,29 @@
+public class main5 {
+  public static void main(String[] args) {
+    // 変数nameに、好きな名前を代入してください
+    String name = "sayaka";
+    
+    // 変数ageに、好きな年齢を代入してください
+    int age = 24;
+    
+    // 変数heightに、好きな身長を代入してください（例: 1.2）
+    double height = 1.5;
+    
+    // 変数weightに、好きな体重を代入してください（例: 20.0）
+    double weight = 50.0;
+    
+    // 変数bmiに、BMIを計算して代入してください
+    double bmi = weight / height / height;
+    
+    // 名前、年齢、身長、体重、BMIに関する情報を出力してください
+    System.out.println("名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    System.out.println("BMIは" + bmi + "です");
+    
+  }
+}
+
+
+// 整数型 総合練習
