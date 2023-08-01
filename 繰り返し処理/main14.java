480d712143b034ad53702855d2ef915a8e04eff0
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-01 17:54:09 +0900

    for文

---
+++ 繰り返し処理/main14.java
@@ -0,0 +1,13 @@
+package 繰り返し処理;
+  class main14 {
+    public static void main(String[] args) {
+      // for文を用いて、繰り返し処理をつくってください
+      for(int i = 1; i <= 10; i++) {
+        System.out.println(i + "回目のループです");
+      }
+      
+    }
+  }
+
+
+  // for文
