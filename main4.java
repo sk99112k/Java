084ab6c0140d61d2a019c7acb884afe89d8c4498
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-08-01 06:33:56 +0000

    強制型変換

---
+++ main4.java
@@ -0,0 +1,13 @@
+public class main4 {
+  public static void main(String[] args) {
+    int number1 = 7;
+    int number2 = 2;
+    System.out.println(number1 / number2);
+    
+    // number1をdouble型にキャストし、number2で割った値を出力してください
+    System.out.println((double)number1 / number2);
+    
+  }
+}
+
+// 強制型変換
