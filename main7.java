bae7add6e241339cff64437ec261dbd41ccd1acf
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-08-01 06:35:23 +0000

    真偽値(比較演算子)

---
+++ main7.java
@@ -0,0 +1,13 @@
+public class main7 {
+  public static void main(String[] args) {
+    // 「4 + 2」と「6」を比較し、falseとなるようにしてください
+    System.out.println(4 + 2 < 6);
+    
+    // 「4 + 2」と「6」を比較し、trueとなるようにしてください
+    System.out.println(4 + 2 <= 6);
+    
+  }
+}
+
+
+// 真偽値(比較演算子)
