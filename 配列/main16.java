8608dd4ccbc0ed9a18b85906492d62113c66c28d
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-01 18:11:24 +0900

    配列の変数定義

---
+++ 配列/main16.java
@@ -0,0 +1,17 @@
+package 配列;
+  class main16 {
+    public static void main(String[] args) {
+      // 変数namesに、配列を代入してください
+      String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+      
+      // インデックス番号が0の要素を出力してください
+      System.out.println(names[0]);
+      
+      // インデックス番号が2の要素を出力してください
+      System.out.println(names[2]);
+      
+    }
+  }
+
+
+  // 配列の変数定義
