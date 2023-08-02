06eeb7fbf5f8609306f53aec3cc879f8de2813ce
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-02 20:55:54 +0900

    配列と繰り返し(for文)

---
+++ 配列/main18.java
@@ -0,0 +1,15 @@
+package 配列;
+class main18 {
+  public static void main(String[] args) {
+    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+    
+    // for文を用いて、「私の名前は◯◯です」と出力してください
+    for (int i = 0; i < names.length; i++) {
+      System.out.println("私の名前は" + names[i] + "です");
+    }
+    
+  }
+}
+
+
+// 配列と繰り返し(for文)
