7cc50088a8b891c535a1504bedbf2efb306d685a
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-02 21:01:48 +0900

    拡張for文

---
+++ 配列/main19.java
@@ -0,0 +1,15 @@
+package 配列;
+class main19 {
+  public static void main(String[] args) {
+    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+    
+    // 配列用のfor文を用いて、「私の名前は◯◯です」と出力してください
+    for(String name: names) {
+      System.out.println("私の名前は" + name + "です");
+    }
+    
+  }
+}
+
+// 拡張for文
+// 要素自体が抽出される
