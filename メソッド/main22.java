d0d9a7243af736e071318ec6c54e5fe291e8eb3f
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-03 16:45:22 +0900

    メソッドの定義

---
+++ メソッド/main22.java
@@ -0,0 +1,16 @@
+package メソッド;
+public class main22 {
+  public static void main(String[] args) {
+    printData();
+  }
+    
+  // printDataメソッドを呼び出してください
+  public static void printData() {
+    // printDataメソッドを定義してください
+    System.out.println("私の名前はKate Jonesです");
+  
+  }
+}
+
+
+// メソッドの定義
