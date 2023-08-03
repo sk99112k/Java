ea95362ad08429b4f8f76d897205517517f261dd
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-03 16:35:12 +0900

    メソッドの呼び出し

---
+++ メソッド/main21.java
@@ -0,0 +1,14 @@
+package メソッド;
+  class main21 {
+    public static void main(String[] args) {
+      hello();
+    }
+    
+    public static void hello() {
+      // "Hello World"を、"Hello Java"に書き換えてください
+      System.out.println("Hello Java");
+    }
+  }
+
+
+// メソッドの呼び出し
