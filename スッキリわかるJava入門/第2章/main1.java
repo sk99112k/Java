2e197d02e1f78e93f7d3f913d93fbc1ad10617b7
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-05 20:09:53 +0900

    エスケープシーケンス

--- スッキリわかるJava入門/第2章/main1.java
+++ スッキリわかるJava入門/第2章/main1.java
@@ -1,5 +1,8 @@
 package スッキリわかるJava入門.第2章;
-
 public class main1 {
-  
+  public static void main(String[] args) {
+    System.out.println("私の好きな記号は(")です");
+                 // 「私の好きな記号は(」が文字列とみなされる。
+                //  残りの部分は文字列とみなされずに、コンパイルエラーになる
+  }
 }
