5366ddbd71c2b92bf81c21774aeca7fbb1efc906
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-05 20:36:52 +0900

    代入次の自動型変換

---
+++ スッキリわかるJava入門/第2章/main4.java
@@ -0,0 +1,9 @@
+package スッキリわかるJava入門.第2章;
+public class main4 {
+  public static void main(String[] args) {
+    float f = 3;
+    double d = f;
+    System.out.println(f); /* float型の変数にint型を代入 */
+    System.out.println(d); /* double型の変数にfloat型を代入 */
+  }
+}
