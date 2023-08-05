8af90ada34b92e21bd21f5daccb23b39b6a812a8
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-05 16:15:46 +0900

    計算

--- スッキリわかるJava入門/第0章/main1.java
+++ スッキリわかるJava入門/第0章/main1.java
@@ -1,9 +1,7 @@
 package スッキリわかるJava入門.第0章;
 public class main1 {
-  public class Main {
-    public static void main(String[] args) { 
-      System.out.println("hello, world!");
-      System.out.println("菅原");
-    }
+  public static void main(String[] args) { 
+    System.out.println("hello, world!");
+    System.out.println("菅原");
   }
 }
---
+++ スッキリわかるJava入門/第0章/main2.java
@@ -0,0 +1,7 @@
+package スッキリわかるJava入門.第0章;
+public class main2 {
+  public static void main(String[] args) {
+    System.out.println("31 + 31の計算をします");
+    System.out.println(31 + 31);
+  }
+}
