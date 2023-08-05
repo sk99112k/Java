c8421393e680d3f89b03cf78b521aa8868e2e56a
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-05 20:39:37 +0900

    代入次の自動型変換

--- スッキリわかるJava入門/第2章/main4.java
+++ スッキリわかるJava入門/第2章/main4.java
@@ -6,4 +6,9 @@ public class main4 {
     System.out.println(f); /* float型の変数にint型を代入 */
     System.out.println(d); /* double型の変数にfloat型を代入 */
   }
+
+  public static void main1(String[] args) {
+    int i = 3.2;
+    // 小数点以下をどうしたらいいかわからないため、エラーになる
+  }
 }
