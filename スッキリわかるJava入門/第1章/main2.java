e56e04d74faef1225adaf10d2f0104c24473b50e
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-05 17:29:40 +0900

    変数の再代入

---
+++ スッキリわかるJava入門/第1章/main2.java
@@ -0,0 +1,9 @@
+package スッキリわかるJava入門.第1章;
+public class main2 {
+  public static void main(String[] args) {
+    int age = 20;
+    System.out.println("私の年齢は" + age);
+    age = 31;
+    System.out.println("いや、本当の年齢は" + age + "です");
+  }
+}
