eabc12840be49422aff9cae8f0fd18167788df1a
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-05 20:16:13 +0900

    エスケープシーケンスを用いる

---
+++ スッキリわかるJava入門/第2章/main2.java
@@ -0,0 +1,7 @@
+package スッキリわかるJava入門.第2章;
+public class main2 {
+  public static void main(String[] args) {
+    System.out.println("私の好きな記号は(¥")です");
+                      //「私の好きな記号は(¥")です」まで文字とみなされる
+  }
+}
