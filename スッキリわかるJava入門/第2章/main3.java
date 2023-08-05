ffe923f1d252bc1ec31d66cc28bd53d7820276ca
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-05 20:30:52 +0900

    インクリメント/デクリメント演算子

---
+++ スッキリわかるJava入門/第2章/main3.java
@@ -0,0 +1,10 @@
+package スッキリわかるJava入門.第2章;
+public class main3 {
+  public static void main(String[] args) {
+    int a;
+    a = 100;
+    a++;
+    // aが1だけ増える
+    System.out.println(a);
+  }
+}
