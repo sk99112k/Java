3c5c1fa06fc43c2f643c9848cb0eb4aa64557324
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-05 16:44:32 +0900

    色んな計算

---
+++ スッキリわかるJava入門/第0章/main3.java
@@ -0,0 +1,9 @@
+package スッキリわかるJava入門.第0章;
+public class main3 {
+  public static void main(String[] args) {
+    System.out.println(35 - 10);
+    System.out.println(-5 * 2);
+    System.out.println(6 * 6 * 3.14);
+    System.out.println("答えは" + 64);
+  }
+}
