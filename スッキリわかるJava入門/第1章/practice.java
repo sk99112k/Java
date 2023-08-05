868ad3057ab668ab6cb185a3786dd36dd6bf76fc
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-05 18:01:19 +0900

    第1章　練習問題

---
+++ スッキリわかるJava入門/第1章/practice.java
@@ -0,0 +1,19 @@
+package スッキリわかるJava入門.第1章;
+public class practice {
+  /* 練習1-1 
+   * ア,コンパイラ  イ,インタプリタ  ウ,コード  エ,バイトコード  オ,JVM */
+  /* 練習1-2 */
+  public static void main2(String[] args) {
+    int a = 3;
+    int b = 5;
+    int c = a * b;
+    System.out.println("縦幅3横幅5の長方形の面積は、" + c + "です");
+  }
+
+  /* 練習1-3 */
+  boolean result = true;
+  char oneCharacter = '駆';
+  double pi = 3.14;
+  long longNumber = 314159265853979L;
+  String letter = "敵に15ポイントのダメージを与えた。";
+}
