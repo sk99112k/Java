2b670d21341e39cfe1d855c46d91959e6ac97432
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-07 16:22:27 +0900

    異なる型同士の算術演算

---
+++ スッキリわかるJava入門/第2章/main6.java
@@ -0,0 +1,9 @@
+package スッキリわかるJava入門.第2章;
+public class main6 {
+  public static void main(String[] args) { 
+		double d = 8.5 / 2;
+		long l = 5 + 2L;
+		System.out.println(d);
+		System.out.println(l);
+	}
+}
