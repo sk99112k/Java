610e7168e06cff3f5a15fa2f9216d50ed99f78fb
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-08 10:49:39 +0900

    命令実行文

--- スッキリわかるJava入門/第2章/main9.java
+++ スッキリわかるJava入門/第2章/main9.java
@@ -6,7 +6,7 @@ public class main9 {
     System.out.print("私の名前は");
     System.out.print(name);
     System.out.print("です");
-      
+
   // 2つの値を比較して大きい方の値を代入する int m = Math.max(1, 2);
     int a = 5;
     int b = 3;
@@ -17,10 +17,17 @@ public class main9 {
     String age = "31";
     int n = Integer.parseInt(age);
     System.out.println("私は来年" + (n + 1) + "歳になります");
-    
+
   // 乱数を発生させる int r = new java.util.Random().nextInt(1);
   // 1の数字までの整数がランダムに代入される
     int r = new java.util.Random().nextInt(50);
     System.out.println("あなたは多分、" + r + "歳ですね？");
- }
+      
+  //  キーボードから入力を受け付ける命令
+    System.out.println("あなたの名前を入力してください");
+    String name1 = new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力してください");
+    int age1 = new java.util.Scanner(System.in).nextInt();
+    System.out.println("ようこそ" + age1 + "歳の" + name1 + "さん");
+  }
 }
