f12cff0db3fbbefafdbcb455f04d4dd85b04f2af
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-08 12:51:43 +0900

    練習１

---
+++ スッキリわかるJava入門/第2章/practice.java
@@ -0,0 +1,10 @@
+package スッキリわかるJava入門.第2章;
+public class practice {
+  /* 練習1 */
+  public static void main(String[] args) { 
+    int x = 5;
+    int y = 10;
+    String ans = "x+yは" + (x + y);
+    System.out.println(ans);
+  }
+}
