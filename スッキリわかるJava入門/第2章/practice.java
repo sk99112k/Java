ee9bf6b7520fbbed31333873a75dbb3b6b4dd3d0
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-11 23:32:26 +0900

    練習3

--- スッキリわかるJava入門/第2章/practice.java
+++ スッキリわかるJava入門/第2章/practice.java
@@ -12,5 +12,17 @@ public class practice {
   2,4,5,7
 
   /* 練習3 */
-  
+  public static void main1(String[] args) { 
+    System.out.println("ようこそ占いの館へ");
+    System.out.println("あなたの名前を入力してください");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力してください");
+    String ageString = new java.util.Scanner(System.in).nextLine();
+    int age = Integer.parseInt(ageString);
+    int fortune = new java.util.Random().nextInt(4);
+    fortune++;
+    System.out.println("占いの結果が出ました");
+    System.out.println(ageString + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
+    System.out.println("1: 大吉 2: 中吉 3: 吉 4: 凶");
+  }
 }
