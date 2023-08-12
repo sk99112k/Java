a77234c6557c3f657ceeb62c47f528ccb2400f92
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-12 13:43:51 +0900

    繰り返し

--- スッキリわかるJava入門/第3章/main2.java
+++ スッキリわかるJava入門/第3章/main2.java
@@ -1,4 +1,10 @@
 package スッキリわかるJava入門.第3章;
 public class main2 {
-  
+  public static void main(String[] args) { 
+    boolean doorClose = true;
+    while (doorClose == true){
+        System.out.println("ノックする");
+        System.out.println("1分待つ");
+    }
+  }
 }
