8e3158ba5c26ab1ff05fe779d14a24e60b6cd458
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-12 13:36:34 +0900

    条件分岐と繰り返し

--- スッキリわかるJava入門/第3章/main1.java
+++ スッキリわかるJava入門/第3章/main1.java
@@ -1,4 +1,12 @@
 package スッキリわかるJava入門.第3章;
 public class main1 {
-  
+  public static void main(String[] args) { 
+    boolean tenki = false;
+    if (tenki == true){
+        System.out.println("洗濯をします");
+        System.out.println("散歩に行きます");
+    } else {
+        System.out.println("DVDを見ます");
+    }
+}
 }
