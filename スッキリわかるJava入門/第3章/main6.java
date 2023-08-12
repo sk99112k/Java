cd63687cf1ceae647348842b6786326bee02fe2f
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-12 16:54:51 +0900

    for文のループ変数iの内容を表示する

--- スッキリわかるJava入門/第3章/main6.java
+++ スッキリわかるJava入門/第3章/main6.java
@@ -1,4 +1,8 @@
 package スッキリわかるJava入門.第3章;
 public class main6 {
-  
+  public static void main(String[] args) {
+    for (int i = 0; i < 3; i++) {
+      System.out.println("現在" + (i + 1) +"周目→");
+    }
+  }
 }
