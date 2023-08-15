102b046587a87302f56027495b937cf4f8ad6d47
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-15 14:49:29 +0900

    配列の作成手順

--- スッキリわかるJava入門/第4章/main1.java
+++ スッキリわかるJava入門/第4章/main1.java
@@ -1,7 +1,9 @@
 package スッキリわかるJava入門.第4章;
 public class main1 {
   public static void main (String [] args) {
-    int[] scores;
-    scores = new int[5];
+    int[] scores;         //step1
+    scores = new int[5];  //step2
+
+    int[] numbers = new int[6];  //step1と2を同時に行う
   }
 }
