b0acb65ba9c43eabe78f9f733bc19c6a4e3cecf7
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-15 15:29:48 +0900

    配列の長さの調べ方

---
+++ スッキリわかるJava入門/第4章/main2.java
@@ -0,0 +1,8 @@
+package スッキリわかるJava入門.第4章;
+public class main2 {
+  public static void main (String [] args) {
+    int [] scorers = new int[5];
+    int num = scorers.length;
+    System.out.println("要素の数は"+ num);
+  }
+}
