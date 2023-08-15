251adeca60c7c8196041ac88408130cc6bf4ebae
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-15 16:23:10 +0900

    配列とfor文

---
+++ スッキリわかるJava入門/第4章/main6.java
@@ -0,0 +1,9 @@
+package スッキリわかるJava入門.第4章;
+public class main6 {
+  public static void main (String [] args) {
+    int[] scorers = {20, 40, 10, 50, 80};
+    for (int i = 0; i < scorers.length; i++) {
+      System.out.println(scorers[i]);
+    }
+  }
+}
