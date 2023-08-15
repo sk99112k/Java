e113b82bf2b31684146231ed4204ac9f05981a0c
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-15 15:33:13 +0900

    配列の要素に値を代入

---
+++ スッキリわかるJava入門/第4章/main3.java
@@ -0,0 +1,8 @@
+package スッキリわかるJava入門.第4章;
+public class main3 {
+  public static void main (String [] args) {
+    int [] scorers = new int[5];
+    scorers[1] = 30;
+    System.out.println(scorers[1]);
+  }
+}
