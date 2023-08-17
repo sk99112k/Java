a4ece0fa0b3cc35e831beaccdc5888811f2d7737
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-17 16:45:39 +0900

    ループによる集計

--- スッキリわかるJava入門/第4章/main6.java
+++ スッキリわかるJava入門/第4章/main6.java
@@ -1,9 +1,22 @@
 package スッキリわかるJava入門.第4章;
 public class main6 {
   public static void main (String [] args) {
+    //ループによる全要素の利用
     int[] scorers = {20, 40, 10, 50, 80};
     for (int i = 0; i < scorers.length; i++) {
       System.out.println(scorers[i]);
     }
+
+    //ループによる集計
+    int[] points = {20, 40, 10, 50, 80};
+    int sum = 0;
+    for (int i = 0; i < points.length; i++) {
+      sum += points[i];
+    }
+    int avg = sum / points.length;
+    System.out.println("合計点" + sum);
+    System.out.println("平均点" + avg);
+
+    //添字に対応した情報の利用
   }
 }
