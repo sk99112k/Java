fc6106a3b5ec46ccd0c31ec4a4594946d62ea18f
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-18 13:44:06 +0900

    添字に対応した情報の利用

--- スッキリわかるJava入門/第4章/main6.java
+++ スッキリわかるJava入門/第4章/main6.java
@@ -1,14 +1,14 @@
 package スッキリわかるJava入門.第4章;
 public class main6 {
   public static void main (String [] args) {
-    //ループによる全要素の利用(全て表示)
+//ループによる全要素の利用(全て表示)
     int[] scorers = {20, 40, 10, 50, 80};
     for (int i = 0; i < scorers.length; i++) {
       System.out.println(scorers[i]);
     }
 
-    //ループによる集計
-      //(合計・平均)
+//ループによる集計
+    //(合計・平均)
     int[] points = {20, 40, 10, 50, 80};
     int sum = 0;
     for (int i = 0; i < points.length; i++) {
@@ -17,7 +17,7 @@ public class main6 {
     int avg = sum / points.length;
     System.out.println("合計点" + sum);
     System.out.println("平均点" + avg);
-      //(条件に一致する要素の数を調べる)
+    //(条件に一致する要素の数を調べる)
     int[] numbers = {20, 40, 10, 50, 80};
     int count = 0;
     for (int i = 0; i < numbers.length; i++) {
@@ -27,6 +27,28 @@ public class main6 {
     }
     System.out.println("50点以上の科目の数は:" + count);
 
-    //添字に対応した情報の利用
+//添字に対応した情報の利用
+    int[] seq = new int[10];
+    //塩基配列をランダムに生成
+    for (int i = 0; i < seq.length; i++) {
+      seq[i] = new java.util.Random().nextInt(4);
+    }
+    //生成した塩基配列の記号を表示
+    for (int i = 0; i < seq.length; i++) {
+      switch (seq[i]) {
+        case 0:
+          System.out.print("A ");
+          break;
+        case 1:
+          System.out.print("T ");
+          break;
+        case 2:
+          System.out.print("G ");
+          break;
+        case 3:
+          System.out.print("C ");
+          break;
+      }
+    }
   }
 }
