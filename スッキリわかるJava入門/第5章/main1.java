9ae414a447d12b617b44337115a5aaadde41164e
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-20 20:50:04 +0900

    メソッドの呼び出し

---
+++ スッキリわかるJava入門/第5章/main1.java
@@ -0,0 +1,12 @@
+package スッキリわかるJava入門.第5章;
+public class main1 {
+  public static void main(String[] args) { 
+    System.out.println("メソッドを呼び出します。");
+    hello();
+    System.out.println("メソッドの呼び出しが終わりました。");
+  }
+  
+  public static void hello () {
+    System.out.println("湊さんこんにちは");
+  }
+}
