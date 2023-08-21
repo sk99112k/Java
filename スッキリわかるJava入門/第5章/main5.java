6497e900f91f7582b7437714eb52d87a0ffdbe19
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-21 10:27:52 +0900

    戻り値

--- スッキリわかるJava入門/第5章/main5.java
+++ スッキリわかるJava入門/第5章/main5.java
@@ -7,5 +7,9 @@ public class main5 {
   public static void main(String[] args) {
     int ans = add(100, 200);
     System.out.println("100 + 200 = " + ans);
+
+    //戻り値をそのまま使う
+    System.out.println(add(add(10, 20), add(30, 40)));
+    //最終的に100が表示される
   }
 }
