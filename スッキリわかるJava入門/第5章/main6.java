fac098a3861731293ec85759e0bc786d2d9b12ce
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-22 11:30:58 +0900

    オーバーロード(引き数の型が異なる場合)

--- スッキリわかるJava入門/第5章/main6.java
+++ スッキリわかるJava入門/第5章/main6.java
@@ -1,4 +1,21 @@
 package スッキリわかるJava入門.第5章;
 public class main6 {
+  //一つ目のメソッド
+  public static int add(int x, int y) {
+    return x + y;
+  }
+  //二つ目のメソッド
+  public static double add(double x, double y) {
+    return x + y;
+  }
+  //二つ目のメソッド
+  public static String add(String x, String y) {
+    return x + y;
+  }
   
+  public static void main(String[] args) { 
+    System.out.println(add(10, 20));
+    System.out.println(add(5.5, 2.4));
+    System.out.println(add("hello", "goodmorning"));
+  }
 }
