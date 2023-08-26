71ee46f17a7ec68e8f60d57cbd31967d6c0c7c09
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-08-26 17:53:27 +0900

    ソースファイルを分割

--- スッキリわかるJava入門/第6章/Calc.java
+++ スッキリわかるJava入門/第6章/Calc.java
@@ -3,8 +3,12 @@ public class Calc {
   public static void main(String[] args) {
     int a = 10;
     int b = 2;
-    int total = tasu(a, b);
-    int delta = hiku(a, b);
+      //tasu(),hiku()はCalcLogicに属しているため、
+      // int total = tasu(a, b);
+      // int delta = hiku(a, b);   ではなく、
+    int total = CalcLogic.tasu(a, b);
+    int delta = CalcLogic.hiku(a, b);
+      //と記載する
     System.out.print("足すと" + total + "引くと" + delta);
   }
 }
